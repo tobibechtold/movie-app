b47eb80bfcfc699f9498898d57665c9a54aea37b
Tobias Bechtold <tobias.bechtold@atruvia.de>
2021-11-19 12:59:02 +0100

    add token to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  bearer: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzdmYTI1YjlmZTVlYjc0YWU4ODZiZmNjZGYzODY5NCIsInN1YiI6IjYxOTZmZmMxMTQ5NTY1MDA4ZmY4MzU3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OY-Pr_NWxGDJ9vBYXmo3W3Mp3tYl7JcD4kVmofjIodY'
 };
