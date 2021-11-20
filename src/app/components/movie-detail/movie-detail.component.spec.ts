befc7e1197833563224979b0416c850323611143
Tobias Bechtold <tobias.bechtold@atruvia.de>
2021-11-20 02:46:09 +0100

    fix tests

--- src/app/components/movie-detail/movie-detail.component.spec.ts
+++ src/app/components/movie-detail/movie-detail.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieDetailComponent } from './movie-detail.component';
+import {AppRoutingModule} from "../../app-routing.module";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('MovieDetailComponent', () => {
   let component: MovieDetailComponent;
@@ -8,7 +11,8 @@ describe('MovieDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MovieDetailComponent ]
+      declarations: [ MovieDetailComponent ],
+      imports: [RouterTestingModule, HttpClientTestingModule]
     })
     .compileComponents();
   });
